024192860ad2eac1425f9299a942ec400cf5b9a3
Carolin <carolin@friikod.se>
2025-11-14 21:33:24 +0100

    Nix wrapper for protonhax

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+pkgs.stdenv.mkDerivation {
+  name = "protonhax-nix";
+  src = fetchGit {
+    url = "https://github.com/jcnils/protonhax";
+    rev = "922a7bbade5a93232b3152cc20a7d8422db09c31";
+  };
+
+  doUnpack = false;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cat $src/protonhax > $out/bin/protonhax
+
+    sed -i 's|#!/bin/bash|#!${pkgs.bash}/bin/bash|g' $out/bin/protonhax
+    chmod +x $out/bin/protonhax
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -5,11 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      inherit (pkgs) callPackage;
+    in
+    {
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      packages.x86_64-linux.protonhax-nix = callPackage ./. { };
 
-  };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.protonhax-nix;
+
+    };
 }
