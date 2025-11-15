a38e5a3a7a0fd9751b0446d7937e1c10c63f16d0
linus <linus.f@protonmail.ch>
2025-11-15 14:07:08 +0100

    wrapProgram to make sure all required libraries are in PATH

--- default.nix
+++ default.nix
@@ -9,11 +9,23 @@ pkgs.stdenv.mkDerivation {
 
   doUnpack = false;
 
+  nativeBuildInputs = with pkgs; [
+    makeWrapper
+  ];
+
+  buildInputs = with pkgs; [
+    coreutils
+    gnugrep
+  ];
+
   installPhase = ''
     mkdir -p $out/bin
     cat $src/protonhax > $out/bin/protonhax
 
     sed -i 's|#!/bin/bash|#!/usr/bin/env bash|g' $out/bin/protonhax
     chmod +x $out/bin/protonhax
+
+    wrapProgram $out/bin/protonhax \
+      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep ]}
   '';
 }
