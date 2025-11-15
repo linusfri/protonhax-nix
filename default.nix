d256c5734b04beaa692fecde45f2918d0159bedb
linus <linus.f@protonmail.ch>
2025-11-15 15:13:14 +0100

    Fixes progress

--- default.nix
+++ default.nix
@@ -16,6 +16,7 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     coreutils
     gnugrep
+    python3
   ];
 
   installPhase = ''
@@ -23,9 +24,18 @@ pkgs.stdenv.mkDerivation {
     cat $src/protonhax > $out/bin/protonhax
 
     sed -i 's|#!/bin/bash|#!/usr/bin/env bash|g' $out/bin/protonhax
+    
+    # Fix NixOS Steam runtime poisoning
+    sed -i '/source \$phd\/\$SteamAppId\/env/a\
+\
+# Fix NixOS Steam runtime poisoning\
+export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep pkgs.python3 ]}:/run/current-system/sw/bin:/usr/bin:/bin"\
+unset LD_LIBRARY_PATH\
+unset LD_PRELOAD' $out/bin/protonhax
+    
     chmod +x $out/bin/protonhax
 
     wrapProgram $out/bin/protonhax \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep ]}
+      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep pkgs.python3 ]}
   '';
 }
