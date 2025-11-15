ad6d46fa46b9cc9ce99e174873ecc379a853dadd
linus <linus.f@protonmail.ch>
2025-11-15 16:12:09 +0100

    Fixed script. See description in default.nix

--- default.nix
+++ default.nix
@@ -1,41 +1,32 @@
-{ pkgs, ... }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  steam-run,
+}:
+stdenv.mkDerivation rec {
+  pname = "protonhax";
+  version = "1.0.5";
 
-pkgs.stdenv.mkDerivation {
-  name = "protonhax-nix";
-  src = fetchGit {
-    url = "https://github.com/jcnils/protonhax";
-    rev = "922a7bbade5a93232b3152cc20a7d8422db09c31";
+  src = fetchFromGitHub {
+    owner = "jcnils";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-5G4MCWuaF/adSc9kpW/4oDWFFRpviTKMXYAuT2sFf9w=";
   };
-
-  doUnpack = false;
-
-  nativeBuildInputs = with pkgs; [
-    makeWrapper
-  ];
-
-  buildInputs = with pkgs; [
-    coreutils
-    gnugrep
-    python3
-  ];
 
   installPhase = ''
     mkdir -p $out/bin
-    cat $src/protonhax > $out/bin/protonhax
-
-    sed -i 's|#!/bin/bash|#!/usr/bin/env bash|g' $out/bin/protonhax
-    
-    # Fix NixOS Steam runtime poisoning
-    sed -i '/source \$phd\/\$SteamAppId\/env/a\
-\
-# Fix NixOS Steam runtime poisoning\
-export PATH="${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep pkgs.python3 ]}:/run/current-system/sw/bin:/usr/bin:/bin"\
-unset LD_LIBRARY_PATH\
-unset LD_PRELOAD' $out/bin/protonhax
-    
-    chmod +x $out/bin/protonhax
-
-    wrapProgram $out/bin/protonhax \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.coreutils pkgs.gnugrep pkgs.python3 ]}
+    cp protonhax $out/bin
   '';
+
+  postFixup = ''
+    sed -i '1s/#!/#!${lib.escape ["/"] (lib.getExe steam-run)} /' $out/bin/protonhax
+  '';
+
+  meta = {
+    description = "All credits for this goes to https://github.com/pneg.";
+    homepage = "https://github.com/jcnils/protonhax";
+    mainProgram = "protonhax";
+  };
 }
--- flake.nix
+++ flake.nix
@@ -12,7 +12,10 @@
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
       inherit (pkgs) callPackage;
     in
